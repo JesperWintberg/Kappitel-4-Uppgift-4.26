34fa1231f8e2cb3df03e20e1b75ba5f0d5729c33
TheBlueSoul5188 <hibba04@hotmail.se>
2022-10-21 10:16:58 +0200

    Add project files.

---
+++ Kappitel 4 Uppgift 4.26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+            for (int i =0; i<heltal.Length;i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+
+                if (siffra == 9)
+                {
+                    siffra = 0;
+                }
+                else
+                {
+                    siffra++;
+                }
+                Console.Write(siffra);  
+            }
+        }
+    }
+}
+
